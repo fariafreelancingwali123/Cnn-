4fd1aec4f98c83763fac8125e893f908aeb9c7ab
fariafreelancingwali123 <fariasammer@gmail.com>
2025-05-02 19:07:46 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = new mysqli("localhost", "ualsglfrxs6ep", "yaaey5i9zq8w", "dbn0ymf4n1sabe");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
